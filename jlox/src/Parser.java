6890ca22bcad3d278e961ccd1a02d859505ae34e
vikirna <vikirna@proton.me>
2026-08-01 17:39:08 +0545

    jlox: for loop
    Signed-off-by: vikirna <vikirna@proton.me>

--- jlox/src/Parser.java
+++ jlox/src/Parser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Parser {
@@ -41,6 +42,7 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(TokenType.FOR)) return forStatement(); 
     if (match(TokenType.IF)) return ifStatement();
     if (match(TokenType.WHILE)) return whileStatement();
     if (match(TokenType.PRINT)) return printStatement();
@@ -48,6 +50,47 @@ class Parser {
 
     return expressionStatement();
   }
+
+  private Stmt forStatement() {
+    consume(TokenType.LEFT_PAREN, "Expect '(' after for.");
+
+    Stmt initializer;
+    if (match(TokenType.SEMICOLON)) {
+      initializer = null;
+    } else if (match(TokenType.VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(TokenType.SEMICOLON)) {
+      condition = expression();
+    }
+    consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+
+    Expr increment = null;
+    if (!check(TokenType.RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(TokenType.RIGHT_PAREN, "Expect ';' after clauses.");
+
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+    }
+
+    if (condition == null) condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    if (initializer != null) {
+      body = new Stmt.Block(Arrays.asList(initializer, body));
+    }
+
+    return body;
+  }
+  
 
   private Stmt whileStatement() {
     consume(TokenType.LEFT_PAREN, "Expect '(' after while.");
